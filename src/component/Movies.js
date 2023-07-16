657deab534c22984a01779574eed54292f2927e0
Nigohmahram <usmonovmaxsudovich@gmail.com>
2023-07-16 16:53:04 +0900

    Add 404 page template

--- src/component/Movies.js
+++ src/component/Movies.js
@@ -5,9 +5,21 @@ export default function Movies(props) {
 
   return(
     <div className="movies ">
-      {movies.length ? movies.map(movie => (
-        <Movie key={movie.imdbID} {...movie} />
-      )) : <h4>Nothing found</h4>}
+    {movies.length ? movies.map(movie => (
+      <Movie key={movie.imdbID} {...movie}/>
+    )) :
+    <main>
+    <div>
+  <main>
+    <h1>Sorry!</h1>
+    <p>
+    Either you aren't cool enough to visit this page or it doesn't exist <em>. . . like your social life.</em>
+    </p>
+    <h3>Nothing Found</h3>
+  </main>
+</div>
+    </main>
+}
     </div>
   )
 }
